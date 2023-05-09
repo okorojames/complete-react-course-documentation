471171286de88beb0386b17f660c9b98c6b56de7
Okoro James <okorojameschizaram@gmail.com>
2023-05-09 14:34:18 +0100

    still at section 4 (handling state updates)

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/Expenses/NewExpense/NewExpense";
 function App() {
   const expenses = [
     {
@@ -25,6 +26,7 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
       <Expenses items={expenses} />
+      <NewExpense />
     </div>
   );
 }
